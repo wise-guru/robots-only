87c5845073952d73d26cb4e80a084f423adba314
wise-guru <wise.guru123@gmail.com>
2022-10-27 19:33:09 -0400

    Add modeals

---
+++ models/message.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  text: { required: true, type: String },
+  user: { required: true, type: Object },
+  date: { default: Date.now(), type: Date },
+});
+
+MessageSchema.virtual("date_formated").get(function () {
+  return this.date.toLocaleDateString("en-gb", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minutes: "2-digit",
+  });
+});
+
+const Message = mongoose.model("Message", MessageSchema);
+
+module.exports = Message;
