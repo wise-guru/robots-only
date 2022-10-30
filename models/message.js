28c05af0536d264e5c77da9f9ac6816166da510c
wise-guru <wise.guru123@gmail.com>
2022-10-29 20:47:04 -0400

    Format

--- models/message.js
+++ models/message.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-  text: { required: true, type: String },
-  user: { required: true, type: Object },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true, minLength: 1, maxLength: 50 },
+  text: { type: String, required: true, maxLength: 1000 },
   date: { default: Date.now(), type: Date },
 });
 
--- models/user.js
+++ models/user.js
@@ -5,9 +5,30 @@ const User = mongoose.model(
   "User",
   new Schema({
     username: { required: true, type: String },
-    email: { required: true, type: String },
+    // email: { required: true, type: String },
     password: { required: true, type: String },
-    role: { default: "basic", type: String },
+    member: { type: Boolean, default: false },
+    admin: { type: Boolean, default: false },
+    avatar: {
+      type: String,
+      required: true,
+      enum: ["clank", "baymax", "bender", "r2d2", "wall-e", "courtney"],
+      default: "courtney",
+    },
+    // icon: {
+    //   type: String,
+    //   required: true,
+    //   ref: "Icon",
+    //   enum: [
+    //     "/images/clank.webp",
+    //     "/images/baymax.png",
+    //     "/images/bender.jpeg",
+    //     "/images/r2d2.jpg",
+    //     "/images/wall-e.png",
+    //     "/images/courtney.png",
+    //   ],
+    //   default: "/images/courtney.png",
+    // },
     date: { type: Date, default: Date.now() },
   })
 );
