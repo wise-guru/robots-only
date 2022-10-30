86df5b36c0ec82b4bb6d9ea10b690c92756e4c89
wise-guru <wise.guru123@gmail.com>
2022-10-29 20:48:43 -0400

    Fix typo in import

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const messageController = require("../controllers/messageController");
-const authenitcationController = require("../controllers/authenticationController");
+const authenticationController = require("../controllers/authenticationController");
 const indexController = require("../controllers/indexController");
 
 // Index routes
@@ -11,16 +11,16 @@ router.post("/", messageController.delete_message_post);
 
 // Sign up routes
 
-router.get("/sign-up", authenitcationController.get_signup);
-router.post("/sign-up", authenitcationController.post_signup);
+router.get("/sign-up", authenticationController.get_signup);
+router.post("/sign-up", authenticationController.post_signup);
 
 router.get("/create-message", messageController.create_get_message);
 router.post("/create-message", messageController.create_message_post);
 
 //Log-in, log-out routes
-router.get("/log-in", authenitcationController.get_login);
-router.post("/log-in", authenitcationController.post_login);
-router.get("log-out", authenitcationController.get_logout);
+router.get("/log-in", authenticationController.get_login);
+router.post("/log-in", authenticationController.post_login);
+router.get("log-out", authenticationController.get_logout);
 
 //Member routes
 
