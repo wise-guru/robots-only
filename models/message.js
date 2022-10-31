7355c45ce8532a6e620acac7a64094f3f1e4d934
wise-guru <wise.guru123@gmail.com>
2022-10-31 00:40:33 -0400

    Add date format virtual

--- models/message.js
+++ models/message.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const MessageSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
@@ -8,14 +9,20 @@ const MessageSchema = new Schema({
   date: { default: Date.now(), type: Date },
 });
 
-MessageSchema.virtual("date_formatted").get(function () {
+MessageSchema.virtual("time").get(function () {
   return this.date.toLocaleDateString("en-gb", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
+    // year: "numeric",
+    // month: "short",
+    // day: "numeric",
     hour: "numeric",
     minutes: "2-digit",
   });
+});
+
+MessageSchema.virtual("date_formatted").get(function () {
+  return this.date
+    ? DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_MED)
+    : "Some point in time";
 });
 
 const Message = mongoose.model("Message", MessageSchema);
