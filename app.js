341df4b107f2041308dd7cb579ea8535393c4858
wise-guru <wise.guru123@gmail.com>
2022-10-31 22:29:22 -0400

    Add app.js

---
+++ app.js
@@ -0,0 +1,86 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+require("dotenv").config();
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
+const session = require("express-session"); // Dependency of passport.js
+
+// Establish database connection
+const mongoose = require("mongoose");
+const mongoDB = process.env.MONGO_KEY;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Import routes
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(compression()); // Compress all routes
+// app.use(helmet());
+app.use(express.static(path.join(__dirname, "public")));
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) return done(err);
+      if (!user) return done(null, false, { message: "Incorrect username" });
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) return done(err);
+        // Passwords match, log user in!
+        if (res) return done(null, user);
+        // Passwords do not match!
+        else return done(null, false, { message: "Incorrect password" });
+      });
+    });
+  })
+);
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) =>
+  User.findById(id, (err, user) => done(err, user))
+);
+
+// Secret value should be a process env value
+app.use(session({ secret: "robots", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+// Access the user object from anywhere in our application
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
