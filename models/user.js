5caba486a3063219afbdf447b9c821e7f957b3c8
wise-guru <wise.guru123@gmail.com>
2022-11-13 00:12:41 -0500

    Add virtuals

--- models/user.js
+++ models/user.js
@@ -1,36 +1,36 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
-const User = mongoose.model(
-  "User",
-  new Schema({
-    username: { required: true, type: String },
-    // email: { required: true, type: String },
-    password: { required: true, type: String },
-    member: { type: Boolean, default: false },
-    admin: { type: Boolean, default: false },
-    avatar: {
-      type: String,
-      required: true,
-      enum: ["clank", "baymax", "bender", "r2d2", "wall-e", "courtney"],
-      default: "courtney",
-    },
-    // icon: {
-    //   type: String,
-    //   required: true,
-    //   ref: "Icon",
-    //   enum: [
-    //     "/images/clank.webp",
-    //     "/images/baymax.png",
-    //     "/images/bender.jpeg",
-    //     "/images/r2d2.jpg",
-    //     "/images/wall-e.png",
-    //     "/images/courtney.png",
-    //   ],
-    //   default: "/images/courtney.png",
-    // },
-    date: { type: Date, default: Date.now() },
-  })
-);
+const userSchema = new Schema({
+  username: { required: true, type: String },
+  // email: { required: true, type: String },
+  password: { required: true, type: String },
+  member: { type: Boolean, default: false },
+  admin: { type: Boolean, default: false },
+  avatar: {
+    type: String,
+    required: true,
+    enum: ["clank", "baymax", "bender", "r2d2", "wall-e", "courtney"],
+    default: "courtney",
+  },
+  date: { default: Date.now(), type: Date },
+});
+
+userSchema.virtual("date_formatted").get(function () {
+  return this.date
+    ? DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_MED)
+    : "Some point in time";
+});
+
+userSchema.virtual("member_status").get(function () {
+  return this.member ? "Yes" : "No";
+});
+
+userSchema.virtual("admin_status").get(function () {
+  return this.admin ? "Yes" : "No";
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
